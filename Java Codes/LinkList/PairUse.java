8f60bebe52ecf51d41eafb62a3141bdb63271a06
Rahul Raj <rahulraj94391@gmail.com>
2021-12-25 16:38:36 +0530

    _

--- Java Codes/LinkList/PairUse.java
+++ Java Codes/LinkList/PairUse.java
@@ -5,10 +5,6 @@ import java.util.ArrayList;
 public class PairUse {
     public static void main(String[] args) {
         
-        ArrayList<Integer> al = new ArrayList<>();
-        al.add(10);
-        al.add(20);
-        al.add(30);
-        al.add(40);
+        
     }
 }
