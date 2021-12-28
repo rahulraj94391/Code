8becf01f18eb57f64ace129b7366b0c1e7ee6fe1
Rahul Raj <rahulraj94391@gmail.com>
2021-12-28 12:00:41 +0530

    nn

--- Java Codes/CB_LinkList/LLUse.java
+++ Java Codes/CB_LinkList/LLUse.java
@@ -4,23 +4,29 @@ import java.util.*;
 
 public class LLUse {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        LinkedList list1 = new LinkedList();
-        list1.addLast(1);
-        list1.addLast(2);
-        list1.addLast(4);
-        LinkedList list2 = new LinkedList();
-        list2.addLast(1);
-        list2.addLast(3);
-        list2.addLast(4);
-        // list1.display();
-        // System.out.println();
-        // list2.display();
-        System.out.println("head 1 " + list1);
-        System.out.println("head 2 " + list2);
+        // Scanner sc = new Scanner(System.in);
+        // LinkedList list1 = new LinkedList();
+        // list1.addLast(1);
+        // list1.addLast(2);
+        // list1.addLast(4);
+        // LinkedList list2 = new LinkedList();
+        // list2.addLast(1);
+        // list2.addLast(3);
+        // list2.addLast(4);
+        // // list1.display();
+        // // System.out.println();
+        // // list2.display();
+        // System.out.println("head 1 " + list1);
+        // System.out.println("head 2 " + list2);
 
-        LinkedList head = mergeTwoLists(list1, list2);
+        StringBuilder sb = new StringBuilder();
+        sb.append("a");
+        System.out.println(sb);
+        sb.toString()
+        
     }
+
+    
 
     
 
--- Java Codes/LinkList/ListNode.java
+++ Java Codes/LinkList/ListNode.java
@@ -29,20 +29,20 @@ import org.w3c.dom.NodeList;
 
 // }
 
-public class ListNode {
-    int val;
-    ListNode next;
+// public class ListNode {
+//     int val;
+//     ListNode next;
 
-    ListNode() {
-    }
+//     ListNode() {
+//     }
 
-    ListNode(int val) {
-        this.val = val;
-    }
+//     ListNode(int val) {
+//         this.val = val;
+//     }
 
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
+//     ListNode(int val, ListNode next) {
+//         this.val = val;
+//         this.next = next;
+//     }
 
-}
+// }
